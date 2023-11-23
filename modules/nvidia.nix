b569777eb21dc6d62efb5a69f833adc016f1c950
Oleksandr Makhmudov <makhmudov.oleksandr@gmail.com>
2023-11-23 14:19:02 +0100

    i still don't remember what changes i make when i commit them

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,10 +1,8 @@
 { pkgs, config, lib, ... }: 
 {
   hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
+    sync.enable = true;
+
     amdgpuBusId = "PCI:6:0:0";
     nvidiaBusId = "PCI:1:0:0";
   };
